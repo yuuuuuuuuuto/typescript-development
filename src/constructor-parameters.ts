04bc41d04da641d53ceaddda6c4ddc69f671cfa1
yuuuuuuuuuto <yuto120925@outlook.jp>
2024-01-08 11:39:55 +0900

    1\8

---
+++ src/constructor-parameters.ts
@@ -0,0 +1,22 @@
+export {};
+class Person {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+let taro = new Person("taro", 43);
+console.log(taro);
+
+type PersonType = typeof Person;
+
+type Profile = ConstructorParameters<PersonType>;
+
+const profile: Profile = ["Ham", 23];
+const ham = new Person(...profile);
+
+console.log(ham);
